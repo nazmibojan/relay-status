64a4afac7b8ac2a28f8faa2f25ab14bc2603f87f
Nazmi Febrian <nazmi.febrian@gmail.com>
2020-01-14 08:33:36 +0700

    Add serial event function

--- relay-status.ino
+++ relay-status.ino
@@ -2,11 +2,18 @@
  * @brief: request relay status to arduino 
  */
 
+#define SERIAL_EVENT
+
 #define RXD2    16
 #define TXD2    17
 #define PERIOD  5000
 
 unsigned long last_request = 0;
+
+#ifdef SERIAL_EVENT
+String inputString = "";
+bool stringComplete = false;
+#endif
 
 void setup() {
   Serial.begin(115200);
@@ -21,8 +28,31 @@ void loop() {
     Serial2.write('\r');
   }
 
+#ifdef SERIAL_EVENT
+  if (stringComplete) {
+    Serial.println(inputString);
+
+    inputString = "";
+    stringComplete = false;
+    
+  }
+#else
   // Check data received from Arduino
   while (Serial2.available()) {
     Serial.print(char(Serial2.read()));
   }
+#endif
 }
+
+#ifdef SERIAL_EVENT
+/* Serial event function */
+void serialEvent() {
+  while (Serial2.available()) {
+    char inChar = (char)Serial2.read();
+    inputString += inChar;
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
+}
+#endif
